69fd1f6d38c1397eeb70dadc42ac209cbeb78fc0
Orlando <ocontreras@buk.cl>
2025-10-21 20:53:20 -0300

    feat: add logs and workers

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from helpers.location_herper import LocationHelper
 
 # Número de workers paralelos (ajusta según tu necesidad)
 # Recomendado: 2-3 workers para empezar y evitar rate limiting del SII
-MAX_WORKERS = 5
+MAX_WORKERS = 7
 
 reader = Reader('data.xls', max_workers=MAX_WORKERS)
 reader.read()
